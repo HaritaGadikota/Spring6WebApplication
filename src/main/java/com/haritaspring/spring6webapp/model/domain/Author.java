b524f11ff0c052a81885553c4a13d03bd81b57ce
Harita <haritagadikota3@gmail.com>
2025-01-09 18:01:45 -0500

    JPA Entities and Entity relations

---
+++ src/main/java/com/haritaspring/spring6webapp/model/domain/Author.java
@@ -0,0 +1,45 @@
+package com.haritaspring.spring6webapp.model.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    //We are creating a relationship between the tables
+    //MappingBy is used to indicate that this side of the relationship is the inverse side
+    //So it means that the books field is managed by the authors field in the Book entity
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/com/haritaspring/spring6webapp/model/domain/Book.java
@@ -0,0 +1,47 @@
+package com.haritaspring.spring6webapp.model.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+//entity says it is a single table named book
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String isbn;
+    //JoinTable - tells that we need the tables to be joined
+    //The @JoinColumn annotation inside joinColumns and inverseJoinColumns tells JPA which foreign key
+    //columns in the join table correspond to the primary keys in the associated entities.
+
+    @ManyToMany
+    @JoinTable(name="author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
