59f834784f6cfe8537041cf9af156693ca6f3eb7
Harita <haritagadikota3@gmail.com>
2025-01-11 11:36:34 -0500

    -Adding equals(), Hashcode(), ToString() Methods

--- src/main/java/com/haritaspring/spring6webapp/model/domain/Author.java
+++ src/main/java/com/haritaspring/spring6webapp/model/domain/Author.java
@@ -2,6 +2,7 @@ package com.haritaspring.spring6webapp.model.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -42,4 +43,43 @@ public class Author {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    //toString() does not convert methods. Instead, it provides a string describing an object’s data.
+    //You override it to customize how your objects are displayed.
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    //Equals() method is used to check if there are any similar objects
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Author author)) return false;
+
+        return Objects.equals(id, author.id);
+    }
+
+    // Generates a numeric code (hash) representing the object, used in hash-based collections like HashMap or HashSet
+    //Two objects with the same equals() should have same hashcode()
+    //Both together ensure objects are correctly handled for uniqueness and efficiently stored.
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/haritaspring/spring6webapp/model/domain/Book.java
+++ src/main/java/com/haritaspring/spring6webapp/model/domain/Book.java
@@ -2,6 +2,7 @@ package com.haritaspring.spring6webapp.model.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -44,4 +45,39 @@ public class Book {
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Book book)) return false;
+
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
